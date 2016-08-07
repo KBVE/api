0e3f7856d2245fbded7d49d703d52353227e9857
seanc <hello@imsean.me>
2016-08-07 12:45:39 -0500

    Removed rethinkdb boilerplate code

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const app = koa();
 const router = require('koa-router')();
 const config = require('./config');
 const routes = require('./routes');
-const rethink = require('rethinkdb');
-const pool = require('koa-rethinkdb-pool');
 
 for (const name in routes) {
   var route = routes[name];
   router[route.method.toLowerCase() || 'get'](route.path, route);
 }
 
-app.use(pool({
-  r: rethink,
-  connectOptions: config.rethink
-}));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
