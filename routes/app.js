db401cf979672f69f89653fb17cb5bc4027419e4
seanc <hello@imsean.me>
2016-08-09 00:38:05 -0500

    Remove test code from app route

--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,8 @@
-var User = require('../models/User');
-
 var app = function* app() {
-  var test = new User({
-    username: 'sean',
-    password: 'prosperity1',
-    email: 'hello@imsean.me'
-  });
-  this.body = yield test.save();
+  this.body = {
+    ok: true,
+    data: 'KBVE API'
+  };
 };
 
 app.method = 'GET';
