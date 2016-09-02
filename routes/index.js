9ae56e4b70d2a1e8a40cbf44304498f1fb7e0bbe
h0lybyte <admin@kbve.com>
2016-09-02 17:56:15 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,7 @@ exports.userGet = require('./user/get');
 exports.userPost = require('./user/post');
 
 exports.session = require('./session');
+
+exports.storage = require('./app/marketplace/storage');
+exports.barcode = require('./app/marketplace/barcode');
+exports.new_item = require('./app/marketplace/new_item');
